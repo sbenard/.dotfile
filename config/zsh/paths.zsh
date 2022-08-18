7f2b75a6cab3bb8f17271fa6d7dadec12e142df7
sbenard <sebastien.benard-ext@mousquetaires.com>
2022-08-18 12:06:28 +0200

    Add stow config

---
+++ config/zsh/paths.zsh
@@ -0,0 +1,12 @@
+#Shortcut for projects
+export WORKSPACE=$HOME/Workspace
+export WORKSHOP=$HOME/Workspace/Workshops
+export DOWNLOAD=$HOME/Downloads
+
+alias work="cd $WORKSPACE;"
+alias workshop="cd $WORKSHOP;"
+alias projects="cd $WORKSPACE/Projects;"
+alias sandbox="cd $WORKSPACE/Sandbox;"
+alias code-zsh="code $ZSH_CUSTOM"
+alias download="cd $DOWNLOAD"
+
